19ed215025b0e044781e66f22f3798f127ddb01a
Daniel-Celtica <85639347+Daniel-Celtica@users.noreply.github.com>
2021-12-13 11:18:27 -0300

    Update barc.js

--- src/components/BarC/barc.js
+++ src/components/BarC/barc.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useCallback } from 'react';
 
-import { BarChart, ResponsiveContainer, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceLine, Brush, Cell} from "recharts";
+import { BarChart, ResponsiveContainer, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceLine, Cell} from "recharts";
 
 const barcolors = ["#B14545", "#E9524C", "#F97F3C", "#FFCD48", "#FFF05F", "#B2D645", "#568953", "#337571", "#4661A9", "#5F4878", "#8A4E6E", "#E274B6"]
 
@@ -48,9 +48,8 @@ export default function BarC (dados){
     <label>{data.name}</label>
     <ResponsiveContainer height={200}>
     <BarChart   
-      // data={data.data} 
       data={ selectedData ? (activeItem) : (data.data)}
-      margin={{ top: 20, right: 0, left: -30, bottom: 0 }}
+      margin={{ top: 20, right: 0, left: -25, bottom: 0 }}
       >
       <CartesianGrid  />
       <XAxis dataKey="name" fontSize="12" interval={0}/>
@@ -69,10 +68,6 @@ export default function BarC (dados){
       ) : (
         null
       )) : ( null) }
-      {/*no click da barra pegar todos os dados da barra (é possivle?) e setar um valor booleano
-        criar um array com os dados e alterar com renderização condicional o grafico
-
-      */}
     </BarChart>
     </ResponsiveContainer>
     <div className="legenda">
